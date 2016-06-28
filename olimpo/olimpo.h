c38e8c42ef3dacf6f1725655d6544f40e1127e8f
cemturker <cem.turker@argela.com.tr>
2016-06-28 21:45:23 +0300

    Initial Commit

---
+++ olimpo/olimpo.h
@@ -0,0 +1,19 @@
+//
+//  olimpo.h
+//  olimpo
+//
+//  Created by Argela on 28/06/16.
+//  Copyright © 2016 olim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for olimpo.
+FOUNDATION_EXPORT double olimpoVersionNumber;
+
+//! Project version string for olimpo.
+FOUNDATION_EXPORT const unsigned char olimpoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <olimpo/PublicHeader.h>
+
+
